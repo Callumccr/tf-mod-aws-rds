762ecdf31c2a554f476d0d5c9dfbf1c88dc24ed3
callumccr1 <callumccr@gmail.com>
2019-07-30 12:18:47 +1200

    test

--- label.tf
+++ label.tf
@@ -7,30 +7,30 @@ module "rds_label" {
 }
 module "subnet_group_label" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context            = var.context
-  attributes         = ["rds-subnet-group"]
+  context            = module.rds_label.context
+  attributes         = ["subnet-group"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
 module "rds_param_group" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context            = var.context
-  attributes         = ["rds-pg"]
+  context            = module.rds_label.context
+  attributes         = ["parameter-group"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
 
 module "rds_option_group" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context            = var.context
-  attributes         = ["rds-og"]
+  context            = module.rds_label.context
+  attributes         = ["option-group"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
 module "rds_sg" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context            = var.context
-  attributes         = ["rds-sg"]
+  context            = module.rds_label.context
+  attributes         = ["sg"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "default" {
 
   db_subnet_group_name        = join("", aws_db_subnet_group.default.*.name)
   parameter_group_name        = length(var.parameter_group_name) > 0 ? var.parameter_group_name : join("", aws_db_parameter_group.default.*.name)
-  option_group_name           = length(var.option_group_name) > 0 ? var.option_group_name : join("", aws_db_option_group.default.*.name)
+  option_group_name           = aws_db_option_group.default.*.name
   license_model               = var.license_model
   multi_az                    = var.multi_az
   storage_type                = var.storage_type
