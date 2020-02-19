8c4a397f00143d798d05250a3f5138cb1ad05c65
Gitlab <gitlab@eggplant.io>
2020-02-19 11:36:06 +0000

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -101,19 +101,30 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_security_group" "default" {
-  count       = var.enabled ? length(var.security_group_ids) : 0
+  count       = var.enabled && ? 1 : 0
   name        = module.rds_sg.id
   description = "Allow inbound traffic from the security groups"
   vpc_id      = var.vpc_id
 
   dynamic "ingress" {
     for_each = var.service_ports
+    iterator = ingress
     content {
       from_port       = ingress.value
       to_port         = ingress.value
       protocol        = "tcp"
-      security_groups = element(var.security_group_ids, count.index)
+      security_groups = length(var.security_group_ids) > 0 ? element(var.security_group_ids, count.index) : null
+    }
+  }
 
+  dynamic "egress" {
+    for_each = var.service_ports
+    iterator = egress
+    content {
+      from_port       = egress.value
+      to_port         = egress.value
+      protocol        = "tcp"
+      security_groups = length(var.security_group_ids) > 0 ? element(var.security_group_ids, count.index) : null
     }
   }
 
