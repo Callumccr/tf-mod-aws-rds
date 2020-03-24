0e871b563d1594f9619f5eec2b29dd0fce57f134
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-24 22:11:33 +0000

    Gitlab README commit

--- label.tf
+++ label.tf
@@ -3,23 +3,27 @@ module "label" {
   namespace          = var.namespace
   environment        = var.environment
   name               = var.name
-  attributes         = ["rds"]
+  attributes         = concat(var.attributes, ["rds"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
 }
 module "subnet_group_label" {
   source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
-  context            = module.label.context
-  attributes         = ["subnet-group"]
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = concat(module.label.attributes, ["subnet-group"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
 }
 module "rds_param_group" {
   source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
-  context            = module.label.context
-  attributes         = ["parameter-group"]
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = concat(module.label.attributes, ["parameter-group"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
@@ -27,16 +31,20 @@ module "rds_param_group" {
 
 module "rds_option_group" {
   source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
-  context            = module.label.context
-  attributes         = ["option-group"]
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = concat(module.label.attributes, ["option-group"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
 }
 module "rds_sg" {
   source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
-  context            = module.label.context
-  attributes         = ["sg"]
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = concat(module.label.attributes, ["sg"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
