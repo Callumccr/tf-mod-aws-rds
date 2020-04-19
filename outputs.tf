29d2a17f3f8bf3a18eba1849c8d9769c8733d4a1
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-04-19 16:32:35 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -48,13 +48,13 @@ output "hostname" {
 }
 
 output "username" {
-  value       = element(aws_db_option_group.default.*.username, 0)
+  value       = aws_db_option_group.default.*.username
   description = "The username  for logging in to the database."
   sensitive   = true
 }
 
 output "password" {
-  value       = element(aws_db_option_group.default.*.password, 0)
+  value       = aws_db_option_group.default.*.password
   description = "The password for logging in to the database."
   sensitive   = true
 }
